2e18137bda9bd1ad54a995faadd50fbae94f2003
Albrecht Nitsche <albrechtnitsche@gmail.com>
2023-08-09 01:29:46 +0200

    make setup-venv.sh usable from outside

--- setup-venv.sh
+++ setup-venv.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+this_script="${BASH_SOURCE[0]}"
+this_script_dir="$(dirname "$this_script")"
+
+
 if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
@@ -10,4 +14,4 @@ fi
 source ./venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install wheel
-python -m pip install -r ./requirements.txt
+python -m pip install -r "$this_script_dir"/requirements.txt
